1b211586318b58676d8ff4b232be2c6b6e945611
Ijtaba Hussain <ijtabahussain@live.com>
2017-01-21 23:08:25 +0000

    Lots of buffoonery I can't be asked to explain

--- spongebob.js
+++ spongebob.js
@@ -2,52 +2,66 @@ var count      = 0;
 var points     = [];
 
 var width      = 1024;
-var height     = 600;
+var height     = 650;
 var segments   = 15;
-var ropeLength = width / segments;
+var armsLength = width / segments;
 
-var renderer   = null;    
-var stage      = null;
-var g          = null;
-var spongebod  = null;
+var renderer     = null;    
+var stage        = null;
+var g            = null;
+var spongebod    = null;
+var sponge_arms  = null;
 
-var spongebod_ripped_factor = 0.462;
+var spongebod_ripped_factor = 6;
+var spongebod_aspect_ratio  = 1300/2000;
+
+var arm_horizontal_offset = 45;
+var arm_vertical_offset   = -100;
+var bod_horizontal_offset = 330;
+var bod_vertical_offset   = 20;
+
+//anti trap
+var squarer_pants = 5;
 
 function start_dancing()
 {
-    renderer = PIXI.autoDetectRenderer(width, height);
+    renderer = PIXI.autoDetectRenderer(width, height, {backgroundColor: 0xffffff});
     document.body.appendChild(renderer.view);
     stage = new PIXI.Container();
 
     for (var i = 0; i < segments; i++)
     {
-        points.push(new PIXI.Point(i * ropeLength,0));
+        points.push(new PIXI.Point(i * armsLength,0));
     }
 
-    var strip = new PIXI.mesh.Rope(PIXI.Texture.fromImage('spongebob_arms.png'), points);
+    sponge_arms = new PIXI.mesh.Rope(PIXI.Texture.fromImage('spongebob_arms.png'), points);
+ 
+    sponge_arms.position.x = arm_horizontal_offset;
+    sponge_arms.position.y = (height / 2) + arm_vertical_offset;
 
-    strip.position.x = 30;
-    strip.position.y = height / 2;
-
-    stage.addChild(strip);
     create_spongebod();
+    stage.addChild(sponge_arms);
 
     g = new PIXI.Graphics();
 
-    g.x = strip.x;
-    g.y = strip.y;
+    g.x = sponge_arms.x;
+    g.y = sponge_arms.y;
     stage.addChild(g);
 
-    // start animating
     animate();
 }
 
-function create_spongebod(){
-    spongebod = PIXI.Sprite.fromImage('spongebob.png');
-    spongebod.x = 361;
-    spongebod.y = 85;
+function create_spongebod(){    
+    spongetex  = PIXI.Texture.fromImage('spongebob.png');
+    spongebod  = new PIXI.mesh.Mesh(spongetex)
 
-    spongebod.scale.x = spongebod_ripped_factor;
+    spongebod.vertices[0] += squarer_pants;
+    spongebod.vertices[2] -= squarer_pants; 
+    
+    spongebod.x = bod_horizontal_offset;
+    spongebod.y = bod_vertical_offset;
+
+    spongebod.scale.x = spongebod_ripped_factor * spongebod_aspect_ratio;
     spongebod.scale.y = spongebod_ripped_factor;
     
     stage.addChild(spongebod);
@@ -55,37 +69,16 @@ function create_spongebod(){
 
 function animate() {
 
-    count += 0.2;
+    count += 1;
 
     for (var i = 0; i < points.length; i++) {
 
-        //points[i].y = (Math.sin((i * 0.5) + count) * 100) ;
-        points[i].x = i * ropeLength;
+        points[i].y = (Math.sin((i * 0.5) + count) * 100) ;
+        points[i].x = i * armsLength;
         
     }
     
     renderer.render(stage);
 
-    renderPoints();
-
     requestAnimationFrame(animate);
 }
-
-function renderPoints () {
-
-    g.clear();
-
-    g.lineStyle(2,0xffc2c2);
-    g.moveTo(points[0].x,points[0].y);
-
-    for (var i = 1; i < points.length; i++) {
-        g.lineTo(points[i].x,points[i].y);
-    };
-
-    for (var i = 1; i < points.length; i++) {
-        g.beginFill(0xff0022);
-        g.drawCircle(points[i].x,points[i].y,10);
-        g.endFill();
-    };
-}
-
